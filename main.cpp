5f9098f98cd213216d8d5cfcc828dff2111957b1
Pawel <25pawel@gmail.com>
2022-06-28 00:32:19 +0200

    Alex Allain - chapter 2 - task 2

--- main.cpp
+++ main.cpp
@@ -2,7 +2,25 @@
 
 using namespace std;
 
+void Tasks_Task1_printName();
+void Tasks_Task2_printFriendsNames();
+
 int main()
 {
+    Tasks_Task1_printName();
+    Tasks_Task2_printFriendsNames();
+}
+
+void Tasks_Task1_printName()
+{
     cout << "My name is Pawel\n";
+}
+
+void Tasks_Task2_printFriendsNames()
+{
+    cout << "Pawel\n";
+    cout << "Kasia\n";
+    cout << "Konrad\n";
+    cout << "Kuba\n";
+    cout << "Mateusz\n";
 }
